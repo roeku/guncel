f1b2e7c80aa599c41d2559ab9056ff92ccfcbac7
Nwevers <ninawevers1991@hotmail.com>
2014-06-25 18:04:14 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,33 @@
+<div class="contained">
+	<div class="ui center aligned three column grid">
+	<div class="row">
+	 <div class="column">
+	 <div class="segment">
+	  guncel 2014&copy;
+	 </div>
+	 </div>
+	 <div class="column">
+	 <div class="segment">
+	  made possible with <i class="html5 icon"></i> <i class="css3 icon"></i>
+	 </div>
+	 </div>
+	 <div class="column">
+	  Built by <a href="http://fdonmez.mdastudent.khlim.be/eportfolio/">Roeku</a> & <a href="">Nw</a>
+	 </div>
+	</div>
+	</div>
+</div>
+<br>
+<script>
+	if(window.chrome) {
+	 $('.banner li').css('background-size', 'cover');
+	}
+	$('.banner').unslider({
+	 fluid: true,
+	 dots: true,
+	 speed: 500
+	});
+</script>
+</body>
+
+</html>
