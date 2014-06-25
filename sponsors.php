6dd065eccfd31c55c080b0802b914baf730587c5
Nwevers <ninawevers1991@hotmail.com>
2014-06-25 18:00:19 +0200

    Create sponsors.php

---
+++ sponsors.php
@@ -0,0 +1,40 @@
+<div class="ui inverted segment borderless">
+<h1 class="ui inverted center aligned header">Sponsored by</h1>
+<div class="ui inverted horizontal icon divider">
+  <i class="circular inverted star basic icon"></i>
+</div>
+<div class="contained">
+<div class="six inverted column stackable ui grid">
+  <div class="column">
+    <div class="ui inverted segment">
+      <img class="overview-image" src="http://www.gundem.be/uploads/images/11/11/23/23/39/1111232339_0_1_017872/111123233953_0_1_017872_4.jpg">
+    </div>
+  </div>
+  <div class="column">
+    <div class="ui inverted segment">
+      <img class="overview-image" src="http://www.gundem.be/uploads/images/11/04/11/22/50/1104112250_0_1_306938/110411225024_0_1_306938_4.jpg">
+    </div>
+  </div>
+  <div class="column">
+    <div class="ui inverted segment">
+      <img class="overview-image" src="http://www.gundem.be/uploads/images/12/02/06/21/03/1202062103_0_1_695956/120206210314_0_1_695956_4.jpg">
+    </div>
+  </div>
+  <div class="column">
+    <div class="ui inverted segment">
+      <img class="overview-image" src="http://www.gundem.be/uploads/images/12/08/06/21/57/1208062157_0_1_156915/120806215756_0_1_156915_4.jpg">
+    </div>
+  </div>
+  <div class="column">
+    <div class="ui inverted segment">
+      <img class="overview-image" src="http://www.gundem.be/uploads/images/12/01/04/16/34/1201041634_0_1_461322/120104163404_0_1_461322_4.jpg">
+    </div>
+  </div>  
+  <div class="column">
+    <div class="ui inverted segment">
+      <img class="overview-image" src="http://www.gundem.be/uploads/images/12/01/04/14/11/1201041411_0_1_120769/120104141101_0_1_120769_4.jpg">
+    </div>
+  </div>
+</div>
+</div>
+</div>
