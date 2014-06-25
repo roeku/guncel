7133366eb286e6a56018599dbc6077b297872d96
Nwevers <ninawevers1991@hotmail.com>
2014-06-25 10:01:41 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,232 @@
+<html>
+<head>
+  <!-- Standard Meta -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+  <!-- Site Properities -->
+  <title>Guncel</title>
+
+  <link href='http://fonts.googleapis.com/css?family=Sanchez' rel='stylesheet' type='text/css'>
+
+  <link rel="stylesheet" type="text/css" href="css/semantic.css">
+  <link rel="stylesheet" type="text/css" href="css/component.css">
+
+  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+  <script src="javascript/semantic.js"></script>
+  <script src="http://unslider.com/unslider.js"></script>
+
+
+</head>
+<body>
+	<?php include ('header.php'); ?>
+<br>
+<div class="contained">
+<h1 class="ui center aligned header">Haberler</h1>
+<div class="ui horizontal icon divider">
+  <i class="browser icon"></i>
+</div>
+<div class="four column doubling ui grid">
+  <div class="column">
+    <div class="ui raised segment">
+    <h2 class="overview-title">Nullam malesuada</h2>
+    <hr>
+    <b>// <a href="">04-10-2015</a></b>
+    <br>
+    <br>
+    <img class="overview-image" src="http://placehold.it/500x220">
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+      <a class="btn" href="">Devamini oku...</a>
+    </div>
+  </div>
+  <div class="column">
+    <div class="ui raised segment">
+    <h2 class="overview-title">Nullam malesuada</h2>
+    <hr>
+    <b>// <a href="">04-10-2015</a></b>
+    <br>
+    <br>
+    <img class="overview-image" src="http://placehold.it/500x220">
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+      <a class="btn" href="">Devamini oku...</a>
+    </div>
+  </div>
+  <div class="column">
+    <div class="ui raised segment">
+    <h2 class="overview-title">Nullam malesuada</h2>
+    <hr>
+    <b>// <a href="">04-10-2015</a></b>
+    <br>
+    <br>
+    <img class="overview-image" src="http://placehold.it/500x220">
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam pzeryzeoiryzeporypozeyr erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+      <a class="btn" href="">Devamini oku...</a>
+    </div>
+  </div>
+  <div class="column">
+    <div class="ui raised segment">
+    <h2 class="overview-title">Nullam malesuada</h2>
+    <hr>
+    <b>// <a href="">04-10-2015</a></b>
+    <br>
+    <br>
+    <img class="overview-image" src="http://placehold.it/500x220">
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+      <a class="btn" href="">Devamini oku...</a>
+    </div>
+  </div>
+  <div class="column">
+    <div class="ui raised segment">
+    <h2 class="overview-title">Nullam malesuada</h2>
+    <hr>
+    <b>// <a href="">04-10-2015</a></b>
+    <br>
+    <br>
+    <img class="overview-image" src="http://placehold.it/500x220">
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+      <a class="btn" href="">Devamini oku...</a>
+    </div>
+  </div>
+  <div class="column">
+    <div class="ui raised segment">
+    <h2 class="overview-title">Nullam malesuada</h2>
+    <hr>
+    <b>// <a href="">04-10-2015</a></b>
+    <br>
+    <br>
+    <img class="overview-image" src="http://placehold.it/500x220">
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+      <a class="btn" href="">Devamini oku...</a>
+    </div>
+  </div>
+  <div class="column">
+    <div class="ui raised segment">
+    <h2 class="overview-title">Nullam malesuada</h2>
+    <hr>
+    <b>// <a href="">04-10-2015</a></b>
+    <br>
+    <br>
+    <img class="overview-image" src="http://placehold.it/500x220">
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+      <a class="btn" href="">Devamini oku...</a>
+    </div>
+  </div>
+</div>
+<div class="ui borderless pagination menu">
+  <a class="item">
+    <i class="left arrow icon"></i> Previous
+  </a>
+  <a class="item">1</a>
+  <a class="item">2</a>
+  <a class="item">3</a>
+  <a class="item">4</a>
+  <a class="item">5</a>
+  <a class="item">6</a>
+  <a class="item">
+    Next <i class="icon right arrow"></i>
+  </a>
+</div>
+</div>
+<div class="ui inverted segment borderless">
+<h1 class="ui inverted center aligned header">Sponsored by</h1>
+<div class="ui inverted horizontal icon divider">
+  <i class="circular inverted star basic icon"></i>
+</div>
+<div class="contained">
+<div class="six inverted column stackable ui grid">
+  <div class="column">
+    <div class="ui inverted segment">
+      <img class="overview-image" src="http://www.gundem.be/uploads/images/11/11/23/23/39/1111232339_0_1_017872/111123233953_0_1_017872_4.jpg">
+    </div>
+  </div>
+  <div class="column">
+    <div class="ui inverted segment">
+      <img class="overview-image" src="http://www.gundem.be/uploads/images/11/04/11/22/50/1104112250_0_1_306938/110411225024_0_1_306938_4.jpg">
+    </div>
+  </div>
+  <div class="column">
+    <div class="ui inverted segment">
+      <img class="overview-image" src="http://www.gundem.be/uploads/images/12/02/06/21/03/1202062103_0_1_695956/120206210314_0_1_695956_4.jpg">
+    </div>
+  </div>
+  <div class="column">
+    <div class="ui inverted segment">
+      <img class="overview-image" src="http://www.gundem.be/uploads/images/12/08/06/21/57/1208062157_0_1_156915/120806215756_0_1_156915_4.jpg">
+    </div>
+  </div>
+  <div class="column">
+    <div class="ui inverted segment">
+      <img class="overview-image" src="http://www.gundem.be/uploads/images/12/01/04/16/34/1201041634_0_1_461322/120104163404_0_1_461322_4.jpg">
+    </div>
+  </div>  
+  <div class="column">
+    <div class="ui inverted segment">
+      <img class="overview-image" src="http://www.gundem.be/uploads/images/12/01/04/14/11/1201041411_0_1_120769/120104141101_0_1_120769_4.jpg">
+    </div>
+  </div>
+</div>
+</div>
+</div>
+<div class="contained">
+<h1 class="ui center aligned header">Yazarlar</h1>
+<div class="ui horizontal icon divider">
+  <i class="pencil icon"></i>
+</div>
+<div class="four column doubling ui grid">
+<div class="column">
+       <img class="ui small left floated image" src="http://placehold.it/220x220">
+                 <h3><a href="">Hüseyin DÖNMEZ</a></h3>
+</div>
+<div class="column">
+       <img class="ui small left floated image" src="http://placehold.it/220x220">
+                 <h3>Hüseyin DÖNMEZ</h3>
+</div><div class="column">
+       <img class="ui small left floated image" src="http://placehold.it/220x220">
+                 <h3>Hüseyin DÖNMEZ</h3>
+</div><div class="column">
+       <img class="ui small left floated image" src="http://placehold.it/220x220">
+                 <h3>Hüseyin DÖNMEZ</h3>
+</div><div class="column">
+       <img class="ui small left floated image" src="http://placehold.it/220x220">
+                 <h3>Hüseyin DÖNMEZ</h3>
+</div><div class="column">
+       <img class="ui small left floated image" src="http://placehold.it/220x220">
+                 <h3>Hüseyin DÖNMEZ</h3>
+</div>
+</div>
+</div>
+<hr>
+<div class="contained">
+	<div class="ui center aligned three column grid">
+	<div class="row">
+	 <div class="column">
+	 <div class="segment">
+	  guncel 2014&copy;
+	 </div>
+	 </div>
+	 <div class="column">
+	 <div class="segment">
+	  made possible with <i class="html5 icon"></i> <i class="css3 icon"></i>
+	 </div>
+	 </div>
+	 <div class="column">
+	  Built by <a href="http://fdonmez.mdastudent.khlim.be/eportfolio/">Roeku</a> & <a href="">Nw</a>
+	 </div>
+	</div>
+	</div>
+</div>
+<br>
+<script>
+	if(window.chrome) {
+	 $('.banner li').css('background-size', 'cover');
+	}
+	$('.banner').unslider({
+	 fluid: true,
+	 dots: true,
+	 speed: 500
+	});
+</script>
+</body>
+
+</html>
