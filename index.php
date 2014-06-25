227417cc511a66fa2c7e3e8e5b2cb79e1e082e65
Nwevers <ninawevers1991@hotmail.com>
2014-06-25 18:02:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,45 +1,17 @@
 <!-- START HEADER -->
 	<?php include ('header.php'); ?>
 <!-- END HEADER -->
+
 <!-- START CONTENT -->
 	<?php include ('content.php'); ?>
 <!-- END CONTENT -->
 
 <!-- START SPONSORS -->
 	<?php include ('sponsors.php'); ?>
-
 <!-- END SPONSORS -->
 
 <!-- START WRITERS -->
-<div class="contained">
-<h1 class="ui center aligned header">Yazarlar</h1>
-<div class="ui horizontal icon divider">
-  <i class="pencil icon"></i>
-</div>
-<div class="four column doubling ui grid">
-<div class="column">
-       <img class="ui small left floated image" src="http://placehold.it/220x220">
-                 <h3><a href="">Hüseyin DÖNMEZ</a></h3>
-</div>
-<div class="column">
-       <img class="ui small left floated image" src="http://placehold.it/220x220">
-                 <h3>Hüseyin DÖNMEZ</h3>
-</div><div class="column">
-       <img class="ui small left floated image" src="http://placehold.it/220x220">
-                 <h3>Hüseyin DÖNMEZ</h3>
-</div><div class="column">
-       <img class="ui small left floated image" src="http://placehold.it/220x220">
-                 <h3>Hüseyin DÖNMEZ</h3>
-</div><div class="column">
-       <img class="ui small left floated image" src="http://placehold.it/220x220">
-                 <h3>Hüseyin DÖNMEZ</h3>
-</div><div class="column">
-       <img class="ui small left floated image" src="http://placehold.it/220x220">
-                 <h3>Hüseyin DÖNMEZ</h3>
-</div>
-</div>
-</div>
-<hr>
+	<?php include ('writers.php'); ?>
 <!-- END WRITERS -->
 
 <!-- START FOOTER -->
