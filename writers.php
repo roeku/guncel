f0078c49339a6fa8ec6ae2167b55d7f8731d5814
Nwevers <ninawevers1991@hotmail.com>
2014-06-25 18:02:21 +0200

    Create writers.php

---
+++ writers.php
@@ -0,0 +1,29 @@
+<div class="contained">
+<h1 class="ui center aligned header">Yazarlar</h1>
+<div class="ui horizontal icon divider">
+  <i class="pencil icon"></i>
+</div>
+<div class="four column doubling ui grid">
+<div class="column">
+       <img class="ui small left floated image" src="http://placehold.it/220x220">
+                 <h3><a href="">Hüseyin DÖNMEZ</a></h3>
+</div>
+<div class="column">
+       <img class="ui small left floated image" src="http://placehold.it/220x220">
+                 <h3>Hüseyin DÖNMEZ</h3>
+</div><div class="column">
+       <img class="ui small left floated image" src="http://placehold.it/220x220">
+                 <h3>Hüseyin DÖNMEZ</h3>
+</div><div class="column">
+       <img class="ui small left floated image" src="http://placehold.it/220x220">
+                 <h3>Hüseyin DÖNMEZ</h3>
+</div><div class="column">
+       <img class="ui small left floated image" src="http://placehold.it/220x220">
+                 <h3>Hüseyin DÖNMEZ</h3>
+</div><div class="column">
+       <img class="ui small left floated image" src="http://placehold.it/220x220">
+                 <h3>Hüseyin DÖNMEZ</h3>
+</div>
+</div>
+</div>
+<hr>
